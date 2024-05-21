62163c06579b84517a28ea1dd3d0683684fc68da
Thomas Dechen <dechendev@gmail.com>
2024-05-21 20:26:19 -0300

    feat: error/success popup and redirection

--- src/app/components/default-signup-layout/default-signup-layout.component.ts
+++ src/app/components/default-signup-layout/default-signup-layout.component.ts
@@ -1,13 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-default-login-layout',
+  selector: 'app-default-signup-layout',
   standalone: true,
   imports: [],
-  templateUrl: './default-login-layout.component.html',
-  styleUrl: './default-login-layout.component.scss'
+  templateUrl: './default-signup-layout.component.html',
+  styleUrl: './default-signup-layout.component.scss'
 })
-export class DefaultLoginLayoutComponent {
+export class DefaultSignupLayoutComponent {
   @Input() title: string = "";
   @Input() primaryBtnText: string = "";
   @Input() secondaryBtnText: string = "";
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,6 +6,11 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
+interface LoginForm {
+  email: FormControl,
+  password: FormControl
+}
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -38,7 +43,10 @@ export class LoginComponent {
 
   submit(){//subscribe pega a resposta da requisição
     this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next: () => this.toastService.success("Login feito com sucesso!"),
+      next: () => {
+        this.toastService.success("Login feito com sucesso!");
+        this.router.navigate(["/user"]);
+      },
       error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
     }) 
     
@@ -46,6 +54,10 @@ export class LoginComponent {
 
   navigate(){
     this.router.navigate(["signup"])
+  }
+
+  entrar(){
+    this.router.navigate(["login"])
   }
 
 }
